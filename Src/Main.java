74484d591ea28f29a9fd84ac724d75f140f66d81
Praskovya09 <126789627+Praskovya09@users.noreply.github.com>
2023-04-04 23:53:11 +0300

    Update Main.java

--- Src/Main.java
+++ Src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int remains =100;
-        int replenishment =1000;
+        int remains = 100;
+        int replenishment = 1000;
         int bonus;
 
         if (replenishment > 999) {
             bonus = replenishment / 100;
-        }else {
+        } else {
             bonus = 0;
         }
 
         int balance = remains + replenishment + bonus;
-        System.out.println( "Ваш баланс: " + balance + " руб. Ваш бонус: " + bonus + " руб.");
+        System.out.println("Ваш баланс: " + balance + " руб. Ваш бонус: " + bonus + " руб.");
     }
 }
